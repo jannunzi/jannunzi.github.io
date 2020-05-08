9860dcb1fd5c5c26f689d39b2c05894989aba295
jannunzi <jannunzi@gmail.com>
2020-05-08 14:49:44 -0400

    added zoom meetings

---
+++ northeastern/2020/summer1/cs4550/index.js
@@ -0,0 +1,18 @@
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return (
+            <button onClick={() => this.setState({ liked: true })}>
+                Like
+            </button>
+        );
+    }
+}
