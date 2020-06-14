fa8b7542995fb27c1ab86d3d677b4e63abf8a462
jannunzi <jannunzi@gmail.com>
2020-06-14 11:02:37 -0400

    added zoom2 meetings

--- northeastern/2020/summer1/cs4550/zoom2.js
+++ northeastern/2020/summer1/cs4550/zoom2.js
@@ -26,7 +26,8 @@ const urls = [
 
   null,
   "https://northeastern.zoom.us/rec/share/48V_E5r5yTtJE4Xjw03tRvIRHN-7X6a80yRPqKEFxEgNYNKSCKtFUD33NBkaT04j",
-  "https://northeastern.zoom.us/rec/share/tM11ALjo02ZJEs_JskDcBvECT6jbeaa80XAZ-KcLnUzKfsSC-yceLe9RlEFxeTz7"
+  "https://northeastern.zoom.us/rec/share/tM11ALjo02ZJEs_JskDcBvECT6jbeaa80XAZ-KcLnUzKfsSC-yceLe9RlEFxeTz7",
+  "https://northeastern.zoom.us/rec/share/6N5EHenBqiBIRZ2Kx0LfV5w9To3eeaa8hyka-PILmWXCPs0VpCbqdFNOopf4Aa8"
 ]
 const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday"]
 let startDate = new Date()
