d4ecbccda36afc7d446456e47f88394654606f31
jannunzi <jannunzi@gmail.com>
2020-06-24 23:21:36 -0400

    testing pwas

---
+++ precache-manifest.e6069a5cd5d282197f12c78c507be023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da305a623b252f92ded602df87633e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb97c8f3e375bd533a35",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6489f78bbc14cf654e7b",
+    "url": "/static/js/2.61893081.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61893081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb97c8f3e375bd533a35",
+    "url": "/static/js/main.bbdc416b.chunk.js"
+  },
+  {
+    "revision": "ccaefecb50f02e1fdd6c",
+    "url": "/static/js/runtime-main.6a369087.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
